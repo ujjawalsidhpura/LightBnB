f4c72f32b9a5ebdf42d7d63aca95db4096ffed2f
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-10-14 18:11:10 +0000

    allproperties func changed to use database and filtering from user input

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -113,12 +113,82 @@ exports.getAllReservations = getAllReservations;
  */
 const getAllProperties = function (options, limit = 10) {
 
-  const dbQuery = `SELECT * FROM properties LIMIT $1`;
-  const values = [limit];
+  let queryParams = [];
 
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  `
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`)
+
+    queryString += `WHERE city LIKE $${queryParams.length}`
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`)
+
+    if (queryParams.length > 1) {
+      queryString += ` AND `
+    } else {
+      queryString += ` WHERE `
+    }
+
+    queryString += ` properties.owner_id = $${queryParams.length}`
+  }
+
+  if (options.minimum_price_per_night) {
+    queryParams.push(`${options.minimum_price_per_night * 100}`)
+
+    if (queryParams.length > 1) {
+      queryString += ` AND `
+    } else {
+      queryString += ` WHERE `
+    }
+
+    queryString += ` properties.cost_per_night > $${queryParams.length} `
+  }
+
+  if (options.maximum_price_per_night) {
+    queryParams.push(`${options.maximum_price_per_night * 100}`)
+
+
+    if (queryParams.length > 1) {
+      queryString += ` AND `
+    } else {
+      queryString += ` WHERE `
+    }
+
+    queryString += ` properties.cost_per_night < $${queryParams.length} `
+  }
+
+  if (options.minimum_rating) {
+    queryParams.push(`${options.minimum_rating}`)
+
+    if (queryParams.length > 1) {
+      queryString += ` AND `
+    } else {
+      queryString += ` WHERE `
+    }
+
+    queryString += ` property_reviews.rating >= $${queryParams.length}`
+  }
+
+  queryParams.push(limit)
+  queryString += `
+  GROUP BY properties.id 
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length}`
+
+  console.log(queryString, queryParams)
   return pool
-    .query(dbQuery, values)
-    .then((res) => { return res.rows })
+    .query(queryString, queryParams)
+    .then((res) => {
+      console.log(res.rows)
+      return res.rows
+    })
     .catch((err) => { console.log(err.message) })
 }
 
