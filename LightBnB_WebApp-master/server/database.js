c35ec2ef1eef9c365dd78132a9c8a44d44c3078a
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-10-14 18:46:31 +0000

    Inserting new properties for user

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -182,13 +182,10 @@ const getAllProperties = function (options, limit = 10) {
   ORDER BY cost_per_night
   LIMIT $${queryParams.length}`
 
-  console.log(queryString, queryParams)
+
   return pool
     .query(queryString, queryParams)
-    .then((res) => {
-      console.log(res.rows)
-      return res.rows
-    })
+    .then((res) => { return res.rows })
     .catch((err) => { console.log(err.message) })
 }
 
@@ -202,11 +199,22 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const dbQuery = `
+  INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces, number_of_bathrooms,number_of_bedrooms,country,street,city,province,post_code)
+  VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14)
+  RETURNING *;
+  `
+  const values = [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms, property.country, property.street, property.city, property.province, property.post_code];
+
+  return pool
+    .query(dbQuery, values)
+    .then((res) => {
+      return res.rows[0]
+    })
+    .catch((err) => console.log(err.message))
 }
+
+
 exports.addProperty = addProperty;
 
 
